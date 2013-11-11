d233987e725647c930dea3c59ae0c2c8c7f91fa0
Marco Tralles <github@qb-it.de>
2013-11-11 20:43:43 +0000

    feinschliff …

--- cms/config/initializers/patch_hash.rb
+++ cms/config/initializers/patch_hash.rb
@@ -1,7 +1,10 @@
 class ::Hash
   def method_missing(name)
-    return self[name] if key? name
-    self.each { |k,v| return v if k.to_s.to_sym == name }
-    super.method_missing name
+    begin
+      return self[name] if key? name
+      self.each { |k,v| return v if k.to_s.to_sym == name }
+      super.method_missing name
+    rescue 
+    end
   end
 end
