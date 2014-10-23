c950afaf7469cc9132216f3489cd4fe8db3c9ed4
Marco Tralles <github@qb-it.de>
2014-10-23 17:53:10 +0200

    * fix migration

--- cms/db/migrate/20141020150040_change_column_type.rb
+++ cms/db/migrate/20141020150040_change_column_type.rb
@@ -2,7 +2,7 @@ class ChangeColumnType < ActiveRecord::Migration
   def up
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == 'mysql'
       change_column :permissions, :subject_id, :integer
-    elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == 'postgres'
+    elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == 'postgresql'
       change_column :permissions, :subject_id, 'integer USING CAST("subject_id" AS integer)'
     end
 
