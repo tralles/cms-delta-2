tralles/cms-delta-2

commit 4d27ee1ed6f630cbb2ccc5a4735074889b6ab8e3

* add initializer * set client to bootstrap * add helper for layout

client/app/models/branch.rb
@@ -30,10 +30,15 @@ class Branch < ActiveRecord::Base
     
 
 
-  def route args = {}
 
-    
-  
+  def route language
+    if route = self.branch_routes.where(:language => language).first
+      return route
+    elsif route = self.branch_routes.first
+      return route
+    else
+      return nil?
+    end
   end
   
   

client/app/assets/stylesheets/application.css
@@ -18,10 +18,12 @@
 #debug
 {
   position: absolute;
+  z-index: 9999999;
   top: 0px;
   right: 0px;
-  opacity: .15;
+  opacity: .5;
   padding: 25px;
+  background: #fee;
 }
 
 #debug:hover

client/app/assets/stylesheets/bootstrap_and_overrides.css.scss
@@ -1,0 +1,70 @@
+
+
+$baseFontSize:          14px;
+$baseLineHeight:        21px;
+
+$baseFontFamily:        'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif !default;
+$altFontFamily:         'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif !default;
+$headingsFontFamily:    'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif !default;
+
+$headingsFontWeight:    200;    // instead of browser default, bold
+
+$headingsColor:         #000;
+$linkColor:             #3399ff;
+
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,200italic');
+
+
+@import "bootstrap";
+@import "bootstrap-responsive";
+
+@import "font-awesome";
+
+
+header
+{
+  margin-bottom: 50px;
+}
+
+a.brand
+{
+  color: #444 !important;
+}
+
+.brand .small
+{
+  font-weight: 100  !important;
+}
+
+.well.navi
+{
+  padding: 0 0 8px 0;
+}
+
+.well.navi .nav-header
+{
+  background: #ddd;
+  color: #333;
+  letter-spacing: 1px;
+  text-shadow: 1px 1px 1px rgba(255, 255, 255, .5);
+}
+
+.well.navi .nav-header a:hover
+{
+  background: transparent;
+}
+
+thin
+{
+  font-weight: 100 !important;
+}
+
+.control-group.datetime select
+{
+  width: auto;
+}
+
+
+.typeahead {
+    z-index: 1100;
+}

client/app/helpers/branches_helper.rb
@@ -1,0 +1,55 @@
+module BranchesHelper
+
+
+  def breadcrump branch, trenner = '<span class="divider"><i class="icon-angle-right"></i></span>'
+    
+    html = ''
+    
+    branch.path.each do |child|
+      
+      route = child.route(@language)
+
+      html << '<li>' 
+      html << link_to(route.name, "/#{@language}#{route.route}")
+      html << trenner
+      html << '</li>' 
+    end
+    
+    return html.html_safe
+  end
+
+
+
+  def menu( branches, nesting = true )
+
+    branches = branches.arrange(:order => :position)
+
+    html = ''
+
+
+    branches.each do |branch, sub_branches|
+
+      html << '<li>'
+
+        route = branch.route(@language)
+        html << link_to(route.name, "/#{@language}#{route.route}")
+                
+        
+        if nesting
+          unless branch.children.empty?
+            html << '<ul class="level">'
+            html << menu(branch.children, nesting)
+            html << '</ul>'
+          end
+        end
+
+      html << '</li>' 
+    end
+
+
+    
+    return html.html_safe
+  end
+
+
+end

client/app/views/application/index.html.haml
@@ -1,43 +1,106 @@
-#debug
-  %fieldset
-    %legend
-      Request
-  
-    %pre
-      = request.protocol
-      = request.host_with_port
-  
-    %pre
-      = request.original_url
-  
-    %pre
-      = params.to_yaml
-  
-  
-  %fieldset
-    %legend
-      Projekt
-    %pre
-      = @project.to_yaml
+
+
+
+.container
+  .row
+    
+    .span12
+      %fieldset
+        %legend
+          Pfad
       
-  
-  %fieldset
-    %legend
-      Branch
-    %pre
-      = @branch.to_yaml
+        %ul.breadcrumb
+          %li 
+            = link_to root_path do
+              = @project.name 
+            %span.divider
+              %i.icon-angle-right
       
-  
-  %fieldset
-    %legend
-      Route
-    %pre
-      = @route.to_yaml
-  
+          = breadcrump @branch
+
+  .row
+    
+    .span12
+      .row
+        .span6
+          %fieldset
+            %legend
+              Main-Navi
+            %ul
+              = menu @project.branches, false
+    
       
+        .span6
+          %fieldset
+            %legend
+              current Sub-Navi
+            %ul
+              = menu @branch.children, true
+              
+  .row
+    
+    .span12
+      %fieldset
+        %legend
+          Contents
+        
+        - @contents.each do |content|
+          - content.content_elements.lang(@locale).each do |ce|
+
+            %strong
+              = ce.content_element_type.name
+            %br
+            = ce.value
+            %br
+            
+
+          %hr        
+      
+          
+- if false
+  %hr
   
-  %fieldset
-    %legend
-      Contents
-    %pre
-      = @contents.to_yaml
+  .container
+  
+    %fieldset
+      %legend
+        Request
+    
+      %pre
+        = request.protocol
+        = request.host_with_port
+    
+      %pre
+        = request.original_url
+    
+      %pre
+        = params.to_yaml
+    
+    
+    %fieldset
+      %legend
+        Projekt
+      %pre
+        = @project.to_yaml
+        
+    
+    %fieldset
+      %legend
+        Branch
+      %pre
+        = @branch.to_yaml
+        
+    
+    %fieldset
+      %legend
+        Route
+      %pre
+        = @route.to_yaml
+    
+        
+    
+    %fieldset
+      %legend
+        Contents
+      %pre
+        = @contents.to_yaml

client/app/views/layouts/application.html.erb
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Client</title>
+  <title><%= @project.name %></title>
   <%= stylesheet_link_tag    "application", media: "all", "data-turbolinks-track" => true %>
   <%= javascript_include_tag "application", "data-turbolinks-track" => true %>
   <%= csrf_meta_tags %>

client/config/initializers/patch_application.rb
@@ -1,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+
+def puts(input, hirb=true)
+  if hirb
+    if input.is_a?(String)
+      super("==> #{input}")
+    else
+      super( Hirb::Helpers::AutoTable.render(input) )
+    end
+  else
+    super(input)
+  end
+end

client/config/initializers/patch_hash.rb
@@ -1,0 +1,7 @@
+class ::Hash
+  def method_missing(name)
+    return self[name] if key? name
+    self.each { |k,v| return v if k.to_s.to_sym == name }
+    super.method_missing name
+  end
+end

commit 0ae8574ce236d5b6b599a0271d2ce4ed230f1e43

client: models/branch: Fix `route()` to return nil
Under certain circumstances, when no branch routes are defined for a
branch, when accessing the menu *Baumstruktur* results in the error below.

```
An ActionView::Template::Error occurred in branches#index:

undefined method `name' for false:FalseClass
app/views/branches/_branch.html.haml:12:in `_app_views_branches__branch_html_haml__4187413576081855020_70172628976900'
[…]
```

Fix up the typo introduced by commit 4d27ee1e (* add initializer, set
client to bootstrap, add helper for layout) by returning `nil`.

Note, the code will still throw an exception.

client/app/models/branch.rb
@@ -30,7 +30,7 @@ class Branch < ActiveRecord::Base
     elsif route = self.branch_routes.first
       return route
     else
-      return nil?
+      return nil
     end
   end
 