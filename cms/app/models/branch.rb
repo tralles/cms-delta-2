tralles/cms-delta-2

commit f488302d07a8f1d406a92deafa12ff081a749e77

zwischenstand …

cms/app/models/branch.rb
@@ -9,9 +9,24 @@ class Branch < ActiveRecord::Base
   accepts_nested_attributes_for :branch_routes, :allow_destroy => true
   
   validates_associated :branch_routes
-  validates_presence_of :name
+
   
   before_destroy :cleanup
+  
+  
+  
+  
+  
+  def route language
+    if route = self.branch_routes.where(:language => language).first
+      return route
+    elsif route = self.branch_routes.first
+      return route
+    else
+      return nil?
+    end
+  end
+  
   
   
   

cms/app/controllers/content_element_types_controller.rb
@@ -30,7 +30,7 @@ class ContentElementTypesController < ApplicationController
 
     respond_to do |format|
       if @content_element_type.save
-        format.html { redirect_to [@project, @content_type], notice: 'Content element type was successfully created.' }
+        format.html { redirect_to project_content_type_path(@project, @content_type), notice: 'Content element type was successfully created.' }
         format.json { render action: 'show', status: :created, location: [@project, @content_type] }
       else
         format.html { render action: 'new' }
@@ -44,7 +44,7 @@ class ContentElementTypesController < ApplicationController
   def update
     respond_to do |format|
       if @content_element_type.update(permitted_params.content_element_type)
-        format.html { redirect_to [@project, @content_type], notice: 'Content element type was successfully updated.' }
+        format.html { redirect_to project_content_type_path(@project, @content_type), notice: 'Content element type was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }

cms/app/controllers/content_types_controller.rb
@@ -29,7 +29,7 @@ class ContentTypesController < ApplicationController
 
     respond_to do |format|
       if @content_type.save
-        format.html { redirect_to project_content_type_content_element_types_path(@project, @content_type), notice: 'Content type was successfully created.' }
+        format.html { redirect_to project_content_type_path(@project, @content_type), notice: 'Content type was successfully created.' }
         format.json { render action: 'show', status: :created, location: @content_type }
       else
         format.html { render action: 'new' }
@@ -43,7 +43,7 @@ class ContentTypesController < ApplicationController
   def update
     respond_to do |format|
       if @content_type.update(permitted_params.content_type)
-        format.html { redirect_to project_content_type_content_element_types_path( @project, @content_type), notice: 'Content type was successfully updated.' }
+        format.html { redirect_to project_content_type_path(@project, @content_type), notice: 'Content type was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }

cms/app/controllers/contents_controller.rb
@@ -15,6 +15,8 @@ class ContentsController < ApplicationController
   # GET /contents/new
   def new
     @content = Content.new
+    @content.project        = @project
+    @content.content_type   = @content_type
   end
   
   def close

cms/app/models/content.rb
@@ -43,6 +43,18 @@ class Content < ActiveRecord::Base
   
   
   
+  def option(name, args = nil)
+    ausgabe = nil
+    
+    meta = YAML::load(self.content_type.meta)
+
+    if meta && meta.send(name.to_sym)
+      ausgabe = meta.send(name.to_sym)
+    end
+      
+    return ausgabe
+
+  end
   
   
   def method_missing(name, args = nil)

cms/app/models/content_element_type.rb
@@ -3,6 +3,7 @@ class ContentElementType < ActiveRecord::Base
   belongs_to :content_type
   has_many :content_elements, :dependent => :destroy
   
+  validates_presence_of :name, :intern
   
   
   def simple_form 

cms/app/models/permitted_params.rb
@@ -30,7 +30,7 @@ class PermittedParams < Struct.new(:params, :current_user)
   end
 
   def content_type_attributes
-    [:ref_id, :name, :intern, :editor]
+    [:ref_id, :name, :intern, :editor, :meta]
   end
   
   

cms/app/views/branches/_branch.html.haml
@@ -2,12 +2,16 @@
 
 - if form && branch == form.object
   %strong 
-    = branch.name
+    = branch.route(@locale).name
+
   %small
     > kann nicht Unterpunkt zu sich oder seinen Unterpunkten sein …
     
 - else
-  = branch.name
+  = branch.route(@locale).name
+  %small.muted
+    = branch.route(@locale).route
+  
 
 - unless form
   .btn-group.pull-right

commit b86e8d81aa95c99a1e4023f56a63fc109d6882de

models/branch: Fix `route()` to return nil
Under certain circumstances, when accessing the menu *Baumstruktur* results in
the error below.

```
An ActionView::Template::Error occurred in branches#index:

  undefined method `name' for false:FalseClass
  app/views/branches/_branch.html.haml:12:in `_app_views_branches__branch_html_haml__4187413576081855020_70172628976900'
[…]
```

Fix up the typo introduced by commit f488302d (zwischenstand …) by
returning `nil`.

Note, the code will still throw an exception.

cms/app/models/branch.rb
@@ -40,7 +40,7 @@ class Branch < ActiveRecord::Base
     elsif route = self.branch_routes.first
       return route
     else
-      return nil?
+      return nil
     end
   end
 